55fb0408232ed62a0669f7c2aba973da188df907
VecTest <vectest@163.com>
2022-01-19 14:14:38 +0800

    First commit

---
+++ Normal.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+void solve()
+{
+
+    return;
+}
+
+int main()
+{
+    int tests;
+
+    scanf("%d", &tests);
+
+    while (tests--)
+    {
+        solve();
+    }
+
+    return 0;
+}
