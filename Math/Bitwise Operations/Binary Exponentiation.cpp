1e6ee9c7d36f20d9b02a275dc21bec4d58c46a9f
VecTest <cn.lixiaoxi@outlook.com>
2022-02-09 22:43:41 +0800

    commit

--- Math/Bitwise Operations/Binary Exponentiation.cpp
+++ Math/Bitwise Operations/Binary Exponentiation.cpp
@@ -6,8 +6,8 @@ T quick_pow(T base, T exponent, T modulo)
     T result = 1;
     while (exponent)
     {
-        if (exponent % 2 == 1) { result = (result % modulo) * (base % modulo) % modulo; }
-        base = (base % modulo) * (base % modulo) % modulo; exponent /= 2;
+        if (exponent % 2 == 1) { result = result * base % modulo; }
+        base = base * base % modulo; exponent /= 2;
     }
     return result % modulo;
 }
