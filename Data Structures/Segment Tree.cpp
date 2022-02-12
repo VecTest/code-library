93bd10617df9a9f47b36e96bf8b28d657c97731a
VecTest <cn.lixiaoxi@outlook.com>
2022-02-12 21:29:47 +0800

    commit

--- Data Structures/Segment Tree.cpp
+++ Data Structures/Segment Tree.cpp
@@ -11,13 +11,13 @@ struct SegmentTree
     {
         int l, r;
         long long tag;
-        long long sum;
+        long long key;
     };
     Node *a;
 
     void push_up(int x)
     {
-        a[x].sum = a[left].sum + a[right].sum;
+        a[x].key = a[left].key + a[right].key;
         return;
     }
 
@@ -25,10 +25,10 @@ struct SegmentTree
     {
         if (a[x].tag != 0 && a[x].l != a[x].r)
         {
-            a[left].sum += a[x].tag * (a[left].r - a[left].l + 1);
+            a[left].key += a[x].tag * (a[left].r - a[left].l + 1);
             a[left].tag += a[x].tag;
 
-            a[right].sum += a[x].tag * (a[right].r - a[right].l + 1);
+            a[right].key += a[x].tag * (a[right].r - a[right].l + 1);
             a[right].tag += a[x].tag;
 
             a[x].tag = 0;
@@ -38,7 +38,10 @@ struct SegmentTree
 
     public:
 
-    SegmentTree() { return; }
+    SegmentTree()
+    {
+        return;
+    }
 
     SegmentTree(int n)
     {
@@ -58,14 +61,14 @@ struct SegmentTree
         return;
     }
 
-    void build(int x, int l, int r, long long *input)
+    void build(int x, int l, int r, const long long *input)
     {
         a[x].l = l, a[x].r = r;
         a[x].tag = 0;
 
         if (l == r)
         {
-            a[x].sum = input[l];
+            a[x].key = input[l];
             return;
         }
 
@@ -77,45 +80,45 @@ struct SegmentTree
         return;
     }
 
-    long long query(int x, int sl, int sr)
+    long long query(int x, const int l, const int r)
     {
-        if (a[x].l >= sl && a[x].r <= sr)
+        if (a[x].l >= l && a[x].r <= r)
         {
-            return a[x].sum;
+            return a[x].key;
         }
 
         long long result = 0;
 
         push_down(x);
-        if (a[left].r >= sl)
+        if (a[left].r >= l)
         {
-            result += query(left, sl, sr);
+            result += query(left, l, r);
         }
-        if (a[right].l <= sr)
+        if (a[right].l <= r)
         {
-            result += query(right, sl, sr);
+            result += query(right, l, r);
         }
 
         return result;
     }
 
-    void modify(int x, int sl, int sr, long long delta)
+    void modify(int x, const int l, const int r, const long long k)
     {
-        if (a[x].l >= sl && a[x].r <= sr)
+        if (a[x].l >= l && a[x].r <= r)
         {
-            a[x].sum += delta * (a[x].r - a[x].l + 1);
-            a[x].tag += delta;
+            a[x].key += k * (a[x].r - a[x].l + 1);
+            a[x].tag += k;
             return;
         }
 
         push_down(x);
-        if (a[left].r >= sl)
+        if (a[left].r >= l)
         {
-            modify(left, sl, sr, delta);
+            modify(left, l, r, k);
         }
-        if (a[right].l <= sr)
+        if (a[right].l <= r)
         {
-            modify(right, sl, sr, delta);
+            modify(right, l, r, k);
         }
         push_up(x);
 
